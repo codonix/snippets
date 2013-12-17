1e55fdf6b061aaf46425621de21f24ff5e3cb6e4
Mohan Srinivasan <mohan.srinivasan.369@gmail.com>
2013-12-17 14:49:16 -0800

    Initial version of perl snippets

---
+++ PerlSnippets.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+
+my $snippet = 1;
+
+##########################
+# Time related
+##########################
+if ($snippet == 1) {
+### Epoch time
+my $now = time();
+print("Epoch Time Now:$now\n");
+}
+#-------------------------
+

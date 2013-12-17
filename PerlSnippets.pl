49f8ecac30dc2e42e0a4a20f0722b481dce09e99
Mohan Srinivasan <mohan.srinivasan.369@gmail.com>
2013-12-17 15:00:01 -0800

    Issue#1 done

--- PerlSnippets.pl
+++ PerlSnippets.pl
@@ -10,6 +10,17 @@ if ($snippet == 1) {
 ### Epoch time
 my $now = time();
 print("Epoch Time Now:$now\n");
+my $gerrit_last_updated = '2013-12-05 10:03:00 PDT';
+my ($sec,$min,$hour,$day,$mon,$year);
+if ($gerrit_last_updated =~ m/([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}).*$/) {
+    $year=$1; $mon=$2;$day=$3;$hour=$4;$min=$5;$sec=$6;
+} else {
+    print("Time in Gerrit has wrong format.\n");
+    exit 1;
 }
-#-------------------------
+use Time::Local;
+$mon = $mon-1;
+my $time = timelocal($sec,$min,$hour,$day,$mon,$year); 
+print("Human:$year-$mon-$day:$hour:$min:$sec Epoch:$time\n");
+}#-------------------------
 
